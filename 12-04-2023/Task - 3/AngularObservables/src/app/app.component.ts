8f4a1a9e54d8614624182bd7efc5d3d911adfbaf
mayank23415 <52768803+mayank23415@users.noreply.github.com>
2023-04-12 18:52:38 +0530

    12-04-2023
    This commit contains the partial implementation of observable.

---
+++ 12-04-2023/Task - 3/AngularObservables/src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'AngularObservables';
+  myObservable = new Observable((observer) => {
+    let time = 1000;
+    for (let i = 0; i < 10; i++) {
+      time = time + 1000;
+      //observer.next(i);
+      setTimeout(() => { observer.next(i) }, time)
+    }
+    
+  })
+
+
+  data;
+
+
+  ngOnInit(): void {
+    this.myObservable.subscribe(
+      (val) => {
+        this.data = val;
+      }
+    )
+  }
+}
